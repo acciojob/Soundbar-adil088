b46a310274873950162264c4870c06ca4cee51e3
Mohd Adil <90774182+adil088@users.noreply.github.com>
2025-02-28 11:15:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
 //your JS code here. If required.
+const sounds = ["applause", "boo", "gasp", "tada", "victory", "wrong"];
+
+const buttonsContainer = document.getElementById("buttons");
+
+let currentAudio = null;
+
+sounds.forEach(sound => {
+
+	const button = document.createElement("button");
+	button.classList.add("btn");
+	button.innerText = sound;
+	
+	currentAudio = new Audio(`sounds/${sound}.mp3`);
+        currentAudio.play();
+})
+
+// Create the stop button
+const stopButton = document.createElement("button");
+stopButton.classList.add("stop");
+stopButton.innerText = "stop";
+
+stopButton.addEventListener("click", () => {
+    if (currentAudio) {
+        currentAudio.pause();
+        currentAudio.currentTime = 0;
+        currentAudio = null;
+    }
+});
+
+buttonsContainer.appendChild(stopButton);
